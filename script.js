307c4bc9ea2a2b2baad6d5e5468c96bc7896098d
Dvonn R <56456710+DvonnR@users.noreply.github.com>
2023-11-20 06:19:25 -0500

    Comlpeted conversion code
    added code that will match the correct letter from the user to the created library stored in the array's provided.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ console.log("Connected...");
 
 var sbt = document.getElementById("send");
 
-var data = ["4","13","(","[)","3","|=","6","|-|","|",".]","|<","1","|Y|","/\/","0","|>","O,","|2","5","7","[_]","\/","\v/","}{","`/","2"]
-var alpha = ["A","B","C","D","E","F","G","H","I","J","k","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var data = ["4","13","(","[)","3","|=","6","|-|","|",".]","|<","1","|Y|","/\/","0","|>","O,","|2","5","7","[_]","\/","\v/","}{","`/","2"," ", 1, 2,3,4,5,6,7,8,9,0,"!","@","#","$", "%","^","&","*","(",")","{","}","?","[","]","|",":",";","'","<",",",">",".","+","-","_"]
+var alpha = ["A","B","C","D","E","F","G","H","I","J","k","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," ", 1, 2,3,4,5,6,7,8,9,0,"!","@","#","$", "%","^","&","*","(",")","{","}","?","[","]","|",":",";","'","<",",",">",".","+","-","_"];
 var result = "";
 var final = document.getElementById(output);
-
+var s1 = 
 
 sbt.addEventListener("click", read );
 
@@ -26,25 +26,22 @@ else if(info !== ""){
     console.log(info);
    
 for(var i = 0; i < info.length; i++){
-    var k = 0;
-    do{
-        
-       
-        if(info[i] == alpha[k]){
-            result += data[k];
-            k++;
-            console.log(result);
-            console.log(info[k]);
-        }
-        
-    }
-    while(alpha[k] !== info[i]);
 
- 
+    for(var k = 0; k < alpha.length; k++){
+        console.log("your in  the right loop");
+        
+        if(alpha[k] == info[i])
+        {
+            console.log("youre in the  else if statment");
+            result += data[k];
+            console.log(result);
+            break;
+        }
+    }
+    console.log(info[i]);
     console.log("you in for loop");
 }
 }
     
-
 }
 
